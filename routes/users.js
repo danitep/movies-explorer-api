575bc6a7b517f25daf860e0c956c39fbaee7bbfd
Tepluhin Daniil <daniiltepluhin@rambler.ru>
2024-03-13 11:11:34 +0500

    user-update-corrected

--- routes/users.js
+++ routes/users.js
@@ -11,6 +11,7 @@ router.get('/me', getCurrentUser);
 router.patch('/me', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
+    email: Joi.string().email(),
   }),
 }), updatePorfile);
 
